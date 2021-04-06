800c8f96b73ef37f144d0d1ef882edf1ff58310c
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-04-06 11:23:52 +0900

    Graphics.Cairo.Surfaces.CairoSurfaceT: remove cairoSurfaceFinish

--- src/Graphics/Cairo/Surfaces/CairoSurfaceT.hs
+++ src/Graphics/Cairo/Surfaces/CairoSurfaceT.hs
@@ -1,5 +1,5 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Graphics.Cairo.Surfaces.CairoSurfaceT (CairoSurfaceT, cairoSurfaceFinish) where
+module Graphics.Cairo.Surfaces.CairoSurfaceT (CairoSurfaceT) where
 
 import Graphics.Cairo.Surfaces.CairoSurfaceT.Internal
