06aeebc4308f2e965912c8c302b163a055027ab2
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-04-07 15:52:21 +0900

    Graphics.Cairo.Drawing.TagsAndLinks.hs: ADT: TagName and Attributes

--- src/Graphics/Cairo/Drawing/TagsAndLinks.hs
+++ src/Graphics/Cairo/Drawing/TagsAndLinks.hs
@@ -6,8 +6,14 @@ import Foreign.Ptr
 import Foreign.C.String
 import Data.CairoContext
 
+import qualified Data.Text as T
+
 foreign import ccall "cairo_tag_begin" c_cairo_tag_begin ::
 	Ptr (CairoT s) -> CString -> CString -> IO ()
 
 foreign import ccall "cairo_tag_end" c_cairo_tag_end ::
 	Ptr (CairoT s) -> CString -> IO ()
+
+newtype TagName = TagName T.Text deriving Show
+
+newtype Attributes = Attributes T.Text deriving Show
