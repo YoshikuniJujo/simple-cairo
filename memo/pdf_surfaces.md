7f42cb86fe9ce1d46c903c02cbb235bb70043d39
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2021-04-07 11:23:49 +0900

    Graphics.Cairo.Surfaces.PdfSurfaces: cairoPdfSurfaceCreate for test

---
+++ memo/pdf_surfaces.md
@@ -0,0 +1,15 @@
+PDF Surfaces memo
+=================
+
+todo
+----
+
+* [ ] define `cairoPdfSurfaceCreate` for test
+	+ use `CairoSurfaceT`
+	+ [x] define plain
+	+ [ ] add exception
+	+ [ ] use CairoSurfacePdfT
+* [ ] define `CairoSurfacePdfT`
+	+ [ ] define ADT
+	+ [ ] others
+* [x] `unsafeCairoSurfaceFinish` for test
---
+++ src/Graphics/Cairo/Surfaces/PdfSurfaces.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE BlockArguments #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Graphics.Cairo.Surfaces.PdfSurfaces where
+
+import Foreign.Ptr
+import Foreign.ForeignPtr hiding (newForeignPtr)
+import Foreign.Concurrent
+import Foreign.C.Types
+import Foreign.C.String
+import Control.Monad.ST
+
+import Graphics.Cairo.Surfaces.CairoSurfaceT.Internal
+
+cairoPdfSurfaceCreate :: FilePath -> CDouble -> CDouble -> IO (CairoSurfaceT s RealWorld)
+cairoPdfSurfaceCreate fp w h = CairoSurfaceT <$> withCString fp \cstr -> do
+	p <- c_cairo_pdf_surface_create cstr w h
+	newForeignPtr p $ c_cairo_surface_destroy p
+
+foreign import ccall "cairo_pdf_surface_create" c_cairo_pdf_surface_create ::
+	CString -> CDouble -> CDouble -> IO (Ptr (CairoSurfaceT s ps))
+
+unsafeCairoSurfaceFinish :: CairoSurfaceT s RealWorld -> IO ()
+unsafeCairoSurfaceFinish (CairoSurfaceT fsr) =
+	withForeignPtr fsr c_cairo_surface_finish
